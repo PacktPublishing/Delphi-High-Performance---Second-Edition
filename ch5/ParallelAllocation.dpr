39d6db9865d0befffc38a7f1e05157f68b483dcd
Primož Gabrijelčič <primoz@gabrijelcic.org>
2023-04-02 20:07:55 +0200

    removed ScaleMM from the demo

--- ch5/ParallelAllocation.dpr
+++ ch5/ParallelAllocation.dpr
@@ -2,7 +2,6 @@ program ParallelAllocation;
 
 uses
 //  FastMM4 in 'FastMM\FastMM4.pas',
-//  ScaleMM2 in 'ScaleMM\ScaleMM2.pas',
 //  cmem in 'tbbmalloc\cmem_xe\cmem.pas',
 //  FastMM5 in 'FastMM5\FastMM5.pas',
   Vcl.Forms,
